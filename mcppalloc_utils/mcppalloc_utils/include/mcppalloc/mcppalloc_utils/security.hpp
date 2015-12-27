a0bd0817c4b6371e734050551af1e898d59934e0
Gary Furnish <gfurnish@gfurnish.net>
2015-12-27 06:59:45 -0700

    Add SSE2 support for GCC for secure_zero

--- mcppalloc_utils/mcppalloc_utils/include/mcppalloc/mcppalloc_utils/security.hpp
+++ mcppalloc_utils/mcppalloc_utils/include/mcppalloc/mcppalloc_utils/security.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <emmintrin.h>
+#include <random>
 namespace mcppalloc
 {
   /**
@@ -9,6 +11,23 @@ namespace mcppalloc
 #ifdef _WIN32
     SecureZeroMemory(s, n);
 #else
+#ifndef __clang__
+/*#ifdef __AVX2__
+const __m256 dummy;
+const __m256i zero = _mm256_xor_ps(dummy, dummy);
+volatile __m256i *p_m256 = reinterpret_cast<volatile __m256i *>(s);
+while (n >= sizeof(__m128i)) {
+  *p_m256++ = zero;
+}
+#endif*/
+#ifdef __SSE2__
+    const __m128i zero = _mm_setzero_si128();
+    volatile __m128i *p_m128 = reinterpret_cast<volatile __m128i *>(s);
+    while (n >= sizeof(__m128i)) {
+      *p_m128++ = zero;
+    }
+#endif
+#endif
     volatile size_t *p_sz = reinterpret_cast<volatile size_t *>(s);
     while (n >= sizeof(size_t) * 8) {
       *p_sz++ = 0;
@@ -31,6 +50,58 @@ namespace mcppalloc
       *p++ = 0;
 #endif
   }
+
+  inline bool is_zero(void *v, size_t sz)
+  {
+    auto ptr = reinterpret_cast<uint8_t *>(v);
+    const auto end = ptr + sz;
+    return ::std::all_of(ptr, end, [](auto &c) { return c == 0; });
+  }
+
+  inline void put_unique_seeded_random(void *v, size_t sz)
+  {
+    if (sz % sizeof(uint32_t))
+      throw ::std::runtime_error("Put unique seeded random size must be divisible by  sizeof(uint32_t)");
+    sz /= sizeof(uint32_t);
+    std::mt19937 mt;
+    auto ptr = reinterpret_cast<uint32_t *>(v);
+    const auto end = ptr + sz;
+    while (ptr != end) {
+      *ptr++ = static_cast<uint32_t>(mt());
+    }
+  }
+  inline bool is_unique_seeded_random(void *v, size_t sz)
+  {
+    if (sz % sizeof(uint32_t))
+      throw ::std::runtime_error("Put unique seeded random size must be divisible by  sizeof(uint32_t)");
+    sz /= sizeof(uint32_t);
+    std::mt19937 mt;
+    auto ptr = reinterpret_cast<uint32_t *>(v);
+    const auto end = ptr + sz;
+    while (ptr != end) {
+      if (*ptr++ != static_cast<uint32_t>(mt()))
+        return false;
+    }
+    return true;
+  }
+  inline size_t is_unique_seeded_random_failure_loc(void *v, size_t sz)
+  {
+    if (sz % sizeof(uint32_t))
+      throw ::std::runtime_error("Put unique seeded random size must be divisible by  sizeof(uint32_t)");
+    sz /= sizeof(uint32_t);
+    std::mt19937 mt;
+    auto ptr = reinterpret_cast<uint32_t *>(v);
+    const auto begin = ptr;
+    const auto end = ptr + sz;
+    while (ptr != end) {
+      if (*ptr++ != static_cast<uint32_t>(mt())) {
+        --ptr;
+        return sizeof(uint32_t) * static_cast<size_t>((ptr - begin));
+      }
+    }
+    return ::std::numeric_limits<size_t>::max();
+  }
+
   /**
    * \brief Secure zero a pointer (the address, not the pointed to object).
    **/
