89d649af552c16418d3da9fba9f4331443d67272
Gary Furnish <gfurnish@gfurnish.net>
2016-10-02 18:19:50 -0600

    Test if constexpr

--- mcppalloc/mcppalloc/include/mcppalloc/declarations.hpp
+++ mcppalloc/mcppalloc/include/mcppalloc/declarations.hpp
@@ -4,4 +4,5 @@ namespace mcppalloc
 {
   // type for representing infinite length.
   static constexpr const size_t c_infinite_length = static_cast<size_t>(-1);
+  static constexpr const unsigned int c_debug_level = 0;
 }
